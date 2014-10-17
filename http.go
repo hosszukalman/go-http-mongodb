c0225779a8a915bd1f8ca0d4a6c16332e6282c8b
Kalman Hosszu <hosszu.kalman@gmail.com>
2014-10-17 07:26:42 +0200

    Format code with go's coding standards.

--- http.go
+++ http.go
@@ -1,66 +1,66 @@
 package main
 
 import (
-    "github.com/julienschmidt/httprouter"
-    "net/http"
-    "labix.org/v2/mgo"
-    "labix.org/v2/mgo/bson"
-    "encoding/json"
-    "log"
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"log"
+	"net/http"
 )
 
 func deleteProfile(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    err := mongoCollection.Remove(bson.M{"name": ps.ByName("name")})
-    if err != nil {
-        http.Error(w, err.Error(), 500)
-    }
+	err := mongoCollection.Remove(bson.M{"name": ps.ByName("name")})
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
 }
 
 func getProfile(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    var result *interface{}
-    err := mongoCollection.Find(bson.M{"name": ps.ByName("name")}).One(&result)
-    if err != nil {
-        http.Error(w, err.Error(), 500)
-    } else {
-        data, err := json.Marshal(result)
-        if err != nil {
-            http.Error(w, err.Error(), 500)
-        } else {
-            w.Header().Set("Content-Type", "application/json")
-            w.Write(data)
-        }
-    }
+	var result *interface{}
+	err := mongoCollection.Find(bson.M{"name": ps.ByName("name")}).One(&result)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	} else {
+		data, err := json.Marshal(result)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(data)
+		}
+	}
 }
 
 func saveProfile(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    var m map[string]interface{}
-    err := json.NewDecoder(r.Body).Decode(&m)
-    if err != nil {
-        http.Error(w, err.Error(), 500)
-    }
-    changeInfo, err := mongoCollection.Upsert(bson.M{"name": m["name"]}, &m)
-    if (changeInfo.Updated != 0) {
-        log.Println("Updated")
-    }
-    if err != nil {
-        http.Error(w, err.Error(), 500)
-    }
+	var m map[string]interface{}
+	err := json.NewDecoder(r.Body).Decode(&m)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+	changeInfo, err := mongoCollection.Upsert(bson.M{"name": m["name"]}, &m)
+	if changeInfo.Updated != 0 {
+		log.Println("Updated")
+	}
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
 }
 
 var mongoCollection *mgo.Collection
 
 func main() {
-    session, err := mgo.Dial("localhost")
-    if err != nil {
-        log.Fatal(err)
-    }
-    mongoCollection = session.DB("gohttptest").C("users")
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	mongoCollection = session.DB("gohttptest").C("users")
 
-    router := httprouter.New()
+	router := httprouter.New()
 
-    router.GET("/user/:name", getProfile)
-    router.DELETE("/user/:name", deleteProfile)
-    router.PUT("/user", saveProfile)
+	router.GET("/user/:name", getProfile)
+	router.DELETE("/user/:name", deleteProfile)
+	router.PUT("/user", saveProfile)
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
